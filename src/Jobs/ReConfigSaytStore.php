9b91678821125b6b6163ce3c19ba0c222c0c6fb8
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-05-22 03:48:56 +0600

    View path typo fixed

--- src/Jobs/ReConfigSaytStore.php
+++ src/Jobs/ReConfigSaytStore.php
@@ -20,7 +20,7 @@ class ReConfigSaytStore implements ShouldQueue
         $shopPagePrefix = config('amplify.basic.shop_page_prefix');
         $shopUrl = config('app.url') . '/' . $shopPagePrefix;
 
-        $compiledStoreScript = view('sayt.store-template', compact('shopUrl'));
+        $compiledStoreScript = view('sayt::store-template', compact('shopUrl'));
         file_put_contents(public_path('sayt-store.js'), $compiledStoreScript);
     }
 }
