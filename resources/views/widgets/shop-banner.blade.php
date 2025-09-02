b8d794aafe76e8df62cf394ba670d2fe55dd0d3c
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-09-03 05:28:31 +0600

    shop banner layout done

--- resources/views/widgets/shop-banner.blade.php
+++ resources/views/widgets/shop-banner.blade.php
@@ -1,4 +1,5 @@
 <div {!! $htmlAttributes !!}>
+
     <section class="banner-shop">
         <div class="banner-content d-flex justify-content-between gap-2 w-100 px-5">
             <div class="w-50 text-start">
@@ -16,62 +17,45 @@
                      src="https://i.ibb.co/358YCgHh/da1d0744de3e6dbc75ea514e0d98a06008dde249.png"
                      alt="Product Image">
                 <img class="img-fluid" width="130" height="130"
-                     src="https://i.ibb.co/Fq7QWYgS/15905e84c8f7897a27d4bf50349a97049eed83b6-1.png"" alt="Product
-                Image">
+                     src="https://i.ibb.co/Fq7QWYgS/15905e84c8f7897a27d4bf50349a97049eed83b6-1.png"
+                     alt="Product Image">
                 <img class="img-fluid" width="130" height="130"
                      src="https://i.ibb.co/svz9wBnQ/bf5efd3beddb776fe980c012083aa933df5b718f.png"
                      alt="Product Image">
             </div>
         </div>
     </section>
+
+{{--    <section class="banner-img-bg" data-image="https://i.ibb.co/d0Mgp1sG/AV-Theater-Room-banner.jpg">--}}
+{{--    </section>--}}
+
+{{--    <section class="banner-video">--}}
+{{--        <div class="video-background">--}}
+{{--            <iframe--}}
+{{--                src="https://www.youtube.com/embed/U6fC4Ij608A?autoplay=1&mute=1&loop=1&playlist=U6fC4Ij608A&controls=0&rel=0&showinfo=0"--}}
+{{--                title="YouTube video player" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>--}}
+{{--            </iframe>--}}
+{{--        </div>--}}
+{{--    </section>--}}
+
     @if($showCloseButton)
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span class="text-white" aria-hidden="true">&times;</span>
         </button>
     @endif
 </div>
-<br>
-<!-- BG img ShopBanner Section -->
-<div class="alert alert-dismissible fade show" role="alert">
-    <section class="banner-img-bg">
 
-    </section>
-    @if($showCloseButton)
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span class="text-white" aria-hidden="true">&times;</span>
-        </button>
-    @endif
-</div>
-<br>
+{{--<style>--}}
 
-<!-- video ShopBanner Section -->
-<div class="alert alert-dismissible fade show" role="alert">
-    <section class="banner-video">
-        <div class="video-background">
-            <iframe
-                src="https://www.youtube.com/embed/U6fC4Ij608A?autoplay=1&mute=1&loop=1&playlist=U6fC4Ij608A&controls=0&rel=0&showinfo=0"
-                title="YouTube video player" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>
-            </iframe>
-        </div>
-        @if($showCloseButton)
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span class="text-white" aria-hidden="true">&times;</span>
-            </button>
-        @endif
-    </section>
-</div>
-</div>
-<style>
+{{--    .banner {--}}
+{{--        background: linear-gradient(90deg, #005f73, #0a9396);--}}
+{{--        color: white;--}}
+{{--        padding: 40px 20px;--}}
+{{--    }--}}
 
-    .banner {
-        background: linear-gradient(90deg, #005f73, #0a9396);
-        color: white;
-        padding: 40px 20px;
-    }
-
-    .product-images img {
-        width: 100px;
-        height: auto;
-        margin: 0 10px;
-    }
-</style>
+{{--    .product-images img {--}}
+{{--        width: 100px;--}}
+{{--        height: auto;--}}
+{{--        margin: 0 10px;--}}
+{{--    }--}}
+{{--</style>--}}
