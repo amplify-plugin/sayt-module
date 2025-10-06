a6c2b2b9455a1e0875098ab2a92ec4b54cc3a5d4
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-10-06 23:30:18 +0600

    sayt store configure js added

--- public/js/sayt-store.js
+++ public/js/sayt-store.js
@@ -275,7 +275,7 @@ document.getElementById("question").addEventListener("focus", function () {
                 });
             });
         requirejs.config({
-            baseUrl: '.',
+            baseUrl: '/vendor/easyask-sayt/js',
             paths: {
                 "jquery": "./jquery-3.2.1.min",
                 "jquery/ui": "./jquery-ui.min",
