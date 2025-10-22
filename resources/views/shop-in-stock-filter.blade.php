aa806ec00c97a75b722605188f33ac447e72158f
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-10-23 03:20:57 +0600

    In Stock Filter view change consistancy issue fixed

--- resources/views/shop-in-stock-filter.blade.php
+++ resources/views/shop-in-stock-filter.blade.php
@@ -5,7 +5,7 @@
                data-title="@if($disabled) {{ $disabledPopUp }} @elseif($checked) {{ $checkedPopUp }} @else {{ 'All Products' }} @endif">
             <input type="checkbox" @checked($checked) name="stock"
                    @disabled($disabled)
-                   data-url="{{ frontendShopURL($currentSeoPath, $extraQuery) }}"
+                   data-url="{{ frontendShopURL([$currentSeoPath, ...$extraQuery]) }}"
                    onchange="onInStockChecked($(this));">
             <span @class(["slider", "disabled" => $disabled])></span>
         </label>
